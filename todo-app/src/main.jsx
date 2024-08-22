d0d028a6d400fa2eb1c9cc579eff0c207fcfb43f
numanarshad123 <nomanarshad2005@gmail.com>
2024-08-23 00:18:32 +0500

    ...

---
+++ todo-app/src/main.jsx
@@ -0,0 +1,15 @@
+
+
+
+import React from "react";
+import StudentList from "./StudentList";
+
+function App() {
+  return (
+    <div>
+      <StudentList />
+    </div>
+  );
+}
+
+export default App;
