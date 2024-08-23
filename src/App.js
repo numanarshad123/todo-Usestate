ccbd28a457164fb23dc23afbd148a0d5c285b863
numanarshad123 <nomanarshad2005@gmail.com>
2024-08-23 08:03:07 +0500

    ..

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import Table from './component/Table/Table';
+import Products from './component/Products/Product';
+import Reduce from './component/UseReduce/Reduce';
+import {BrowserRouter,Route,Routes,HashRouter,useNavigate} from "react-router-dom";
+import Demo from './component/Demo/Demo';
+import Tableinput from './component/Create simple input table/Tableinput';
+
+
+function App() {
+  return (
+    <div >
+      <HashRouter >
+        <Routes>
+          <Route path='/table' element={<Table/>}/>
+          <Route path='/' element={<Router_dome/>}/>
+          <Route path='/Products' element={<Products/>}/>
+          <Route path='/demo' element={<Demo/>}/>
+          <Route path='/reduce' element={<Reduce/>}/>
+          <Route path='/simpletable' element={<Tableinput/>}/>
+        </Routes>
+      
+      </HashRouter>
+  
+    </div>
+  );
+}
+
+export default App;
+
+
+function Router_dome(){
+
+  const nevigate=useNavigate()
+  let startroute=(url)=>{
+    nevigate(url)
+  }
+return(
+<div>
+<div className="main-container">
+  <button className='button-to-route' onClick={()=>startroute("/simpletable")}>Simple Input table</button>
+  <button className='button-to-route' onClick={()=>startroute("/table")}>Table I/O delet/Update</button>
+  <button className='button-to-route' onClick={()=>startroute("/Products")}>Products</button>
+  <button className='button-to-route' onClick={()=>startroute("/demo")}>Show data in console 'instant'</button>
+  <button className='button-to-route' onClick={()=>startroute("/reduce")}>UseReduce</button>
+</div>
+</div>
+);
+}
